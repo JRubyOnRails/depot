a49e4b1ea6ddf85e1e9d04e0bef51c91904816dc
Red Maple <M8R-558ciw@safetymail.info>
2012-12-08 14:31:04 -0600

    Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.order(:title)
+  	if session[:kounter].nil?
+  		session[:kounter] = 0
+  	end
+  	session[:kounter] = session[:kounter] + 1
   end
 end
